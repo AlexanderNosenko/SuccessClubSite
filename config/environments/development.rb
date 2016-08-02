a1698012af3b5c9d5348cd000784327a7631e507
Igor Onischenko <optimum.flaminis@gmail.com>
2016-08-02 05:27:30 +0300

    confirmable in development
    use mailcatcher if you want to try

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,8 +18,12 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.default_url_options = { :host => 'localhost:3000' }
   config.action_mailer.delivery_method = :smtp
-  config.action_mailer.smtp_settings = { address: 'localhost',
-                                           port: 1025 }
+  config.action_mailer.smtp_settings = {
+    address: 'localhost',
+    domain: "improf.club",
+    user_name: "support@improf.club",
+    port: 1025
+  }
   # Print deprecation notices to the Rails logger.
   config.active_support.deprecation = :log
 
