87f8f030774b8af4d3cf3cdf88fb6d450ee20cbe
Igor Onischenko <optimum.flaminis@gmail.com>
2016-08-04 18:54:41 +0300

    fixing

--- config/environments/development.rb
+++ config/environments/development.rb
@@ -18,7 +18,7 @@ Rails.application.configure do
   config.action_mailer.raise_delivery_errors = true
   config.action_mailer.delivery_method = :sendmail
   config.action_mailer.sendmail_settings = { :arguments => "-i" }
-  # config.action_mailer.default_url_options = { :host => 'localhost:3000' }
+  config.action_mailer.default_url_options = { :host => 'improf.club' }
   # config.action_mailer.delivery_method = :smtp
   # config.action_mailer.smtp_settings = {
   #   address: 'localhost',
