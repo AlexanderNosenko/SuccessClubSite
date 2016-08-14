7b73d660bc69ceec621ce4b594e376913e03b134
Igor Onischenko <optimum.flaminis@gmail.com>
2016-08-14 18:28:06 +0300

    last fixes

--- app/assets/javascripts/home.js
+++ app/assets/javascripts/home.js
@@ -9,3 +9,17 @@
 //= require jvectormap/jquery-jvectormap-1.2.2.min.js
 //= require jvectormap/jquery-jvectormap-world-mill-en.js
 //= require toastr/toastr.min.js
+function copyToClipboard(element) {
+    var $temp = $("<input>");
+    $("body").append($temp);
+    console.log(element);
+    $temp.val(element.val()).select();
+    document.execCommand("copy");
+    $temp.remove();
+}
+
+$(document).ready(function(){
+  $("#copy_first_link").click(function(){
+    copyToClipboard($("#first-partner-link-input"));
+  });
+});
