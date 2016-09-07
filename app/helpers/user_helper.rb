4b8b16d2dd5495c4d2db67bfa02ae9b69e383dd0
DaniilAnichin <anichindaniil@gmail.com>
2016-09-07 11:47:49 +0300

    Fixed distribute_money helper
    Added wallet auto-create for new users

--- app/helpers/user_helper.rb
+++ app/helpers/user_helper.rb
@@ -1,17 +1,19 @@
 module UserHelper
-    def distibute_money(user, amount)
+    def distribute_money(user, amount)
     	user.ancestors.reverse.each do |ancestor|
-    		max_depth = ancestor.status.partnership_depth
+    		max_depth = ancestor.role.partnership_depth
     		depth = calculate_depth(ancestor, user) 
     		unless depth > max_depth
     			percent = PartnershipDepth.find(depth).percent
     			ancestor.wallet.bonus_balance += amount * percent / 100 
+                ancestor.wallet.save
     		end
     	end
     end
 
     def calculate_depth(user, descendant)
-    	if descendant.ancestry.include? user.ancestry
+        if (user.ancestry.nil? and descendant.ancestry.start_with?(user.id.to_s)) or \
+            descendant.ancestry.include? user.ancestry
     		descendant.depth - user.depth
     	else
     		nil
