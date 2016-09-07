c0991f47c32160eadd8f52ebcaa046d14df2cece
Igor Onischenko <optimum.flaminis@gmail.com>
2016-09-07 16:38:54 +0300

    simple admin/user#show done, reorganized admin/users views

--- app/controllers/admin/users_controller.rb
+++ app/controllers/admin/users_controller.rb
@@ -2,4 +2,8 @@ class Admin::UsersController < Admin::AdminController
   def index
     @users = User.order(created_at: :desc).paginate(:per_page => 15, :page => params[:page])
   end
+  def show
+    @user = User.find params[:id]
+    @descendants = @user.descendants
+  end
 end
