fa34ed500192fe867ec4987f44b32c23e60f6287
DaniilAnichin <anichindaniil@gmail.com>
2016-09-12 20:50:48 +0300

    Default values for wallets, errors from log

--- app/models/wallet.rb
+++ app/models/wallet.rb
@@ -1,4 +1,13 @@
 class Wallet < ActiveRecord::Base
   belongs_to :user
   validates_presence_of :user
+  after_initialize :init
+
+  def init
+  	self.main_balance ||= 0
+  	self.bonus_balance ||= 0
+  	# if self.id < 500:
+  	#   self.bonus_balance ||= 200
+  	# end
+  end
 end
