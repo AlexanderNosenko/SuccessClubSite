ee5c8abb0d36fa2698d7d2804aff536271505b88
Igor Onischenko <optimum.flaminis@gmail.com>
2016-09-14 23:33:31 +0300

    nginx conf sockets moved to /var/sockets and unicorn pids to /var/pids

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -1,8 +1,8 @@
 working_directory "./"
-pid '/tmp/pids/unicorn.pid'
+pid './tmp/pids/unicorn.pid'
 stderr_path './log/unicorn.log'
 stdout_path './log/unicorn.log'
 
-listen '/tmp/sockets/unicorn.sock'
+listen './tmp/sockets/unicorn.sock'
 worker_processes 2
 timeout 30
