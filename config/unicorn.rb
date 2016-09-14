121c936f7e9fc3fcd7de85b617633c4ca293531f
Igor Onischenko <optimum.flaminis@gmail.com>
2016-09-15 02:36:00 +0300

    nginx/unicorn socket name changed
    fix of funny bugs caused by wrong hostname

--- config/unicorn.rb
+++ config/unicorn.rb
@@ -3,6 +3,6 @@ pid '/var/pids/unicorn.pid'
 stderr_path './log/unicorn.log'
 stdout_path './log/unicorn.log'
 
-listen '/var/sockets/unicorn.sock'
+listen '/var/sockets/improf.club.sock'
 worker_processes 2
 timeout 30
