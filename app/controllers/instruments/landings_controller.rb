89ca1542a5d32c3579ed58f27b66d2aee8c0a7ca
DaniilAnichin <anichindaniil@gmail.com>
2016-09-20 18:56:15 +0300

    Fixed landings rebuying

--- app/controllers/instruments/landings_controller.rb
+++ app/controllers/instruments/landings_controller.rb
@@ -26,30 +26,34 @@ class Instruments::LandingsController < ApplicationController
     @price = @landing.price.to_f
     # Need to correct this
     if @wallet.main_balance < @price
-      render plain: "Not enough money"
+      flash[:notice] = "Not enough money"
     else
       @wallet.main_balance -= 10
-      link = UserLanding.new
-      keys = [:video_link, :has_vk, :has_ok, :has_fb, :has_youtube]
-      # parameters = params.slice(*keys)
-      parameters = {video_link: '', has_fb: true, has_ok: true, has_vk: true, has_youtube: true}
-      
-      link.user_id = @user.id
-      link.landing_id = @landing.id
-      link.activated_at = Time.now
-      link.reactivate_at = link.activated_at + 30.days
-      # Need to generate form for this one
-      link.update_attributes(**parameters)
-
-      link_saved = link.save
-      wallet_saved = @wallet.save
-      if link_saved and wallet_saved
-        flash[:notice] = 'Landing is linked successfully'
+      if UserLanding.find_by(user_id: @user.id, landing_id: @landing.id)
+      	flash[:notice] = 'Already bought'
       else
-        flash[:notice] = 'Something went wrong'
+	      link = UserLanding.new
+	      keys = [:video_link, :has_vk, :has_ok, :has_fb, :has_youtube]
+	      # parameters = params.slice(*keys)
+	      parameters = {video_link: '', has_fb: true, has_ok: true, has_vk: true, has_youtube: true}
+	      
+	      link.user_id = @user.id
+	      link.landing_id = @landing.id
+	      link.activated_at = Time.now
+	      link.reactivate_at = link.activated_at + 30.days
+	      # Need to generate form for this one
+	      link.update_attributes(**parameters)
+
+	      link_saved = link.save
+	      wallet_saved = @wallet.save
+	      if link_saved and wallet_saved
+	        flash[:notice] = 'Landing is linked successfully'
+	      else
+	        flash[:notice] = 'Something went wrong'
+	      end
       end
-      redirect_to action: :index
     end
+    redirect_to action: :index
   end
 
   private
