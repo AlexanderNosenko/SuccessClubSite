c6e91b193f77b9933fac79bb747dd681d6249447
alexandr <sania.ace@gmail.com>
2016-09-28 12:16:51 +0100

    liqpay form is visible only for admin and devs

--- app/controllers/finance_api_controller.rb
+++ app/controllers/finance_api_controller.rb
@@ -161,7 +161,7 @@ class FinanceApiController < ApplicationController
       :version      => '3',
       :action       => "pay",
       :amount       => params['amount'],
-      :currency     => "USD",
+      :currency     => "UAH",
       :description  => 'No desc yet',
       :customer     => current_user.id.to_s,
       :server_url   => "http://improf.club/finance_api/responce_status/liqpay",
