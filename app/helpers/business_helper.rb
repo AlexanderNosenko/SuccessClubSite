cbad0c1159a04a7c29c9365914ec4e31866716ca
Igor Onischenko <optimum.flaminis@gmail.com>
2016-10-19 11:35:50 +0300

    added mockup for business#show view
    need to be developed after comments model

--- app/helpers/business_helper.rb
+++ app/helpers/business_helper.rb
@@ -8,6 +8,17 @@ module BusinessHelper
   end
 
   def created_ago business
-  	distance_of_time_in_words Time.now, business.created_at  	
+  	distance_of_time_in_words Time.now, business.created_at
+  end
+
+  def get_stars number
+    text = ""
+    number.times do
+      text += '<i class="fa fa-star fullstar"></i>'
+    end
+    (5 - number).times do
+      text += '<i class="fa fa-star"></i>'
+    end
+    text.html_safe
   end
 end
