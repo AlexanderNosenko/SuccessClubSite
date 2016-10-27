0c3545ccfc87f3b056206bc8fa933cf7cc091445
Igor Onischenko <optimum.flaminis@gmail.com>
2016-10-27 12:59:28 +0300

    statictics v0.1

--- app/controllers/admin/home_controller.rb
+++ app/controllers/admin/home_controller.rb
@@ -1,9 +1,9 @@
 class Admin::HomeController < Admin::AdminController
   def index
-    developers_ids = [2, 56, 61, 85, 1136]
+    developers_ids = [2, 85, 1136, 61, 56, 108, 118, 22, 1053] #[2, 56, 61, 85, 1136]
     @users_money = Wallet.where.not(user_id: developers_ids).sum(:main_balance)
     @got = Payment.where.not(to_user_id: developers_ids).sum(:amount)
-    @withdrawn = Withdrawal.where.not(id: developers_ids).where(status: true).sum(:amount)
+    @withdrawn = Withdrawal.where(status: true).sum(:amount)
     @we_have = @got - @users_money - @withdrawn
   end
 end
