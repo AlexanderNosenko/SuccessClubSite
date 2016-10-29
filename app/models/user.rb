b76365d4aa89632688da50f7b41d189dcc032e05
DaniilAnichin <anichindaniil@gmail.com>
2016-10-29 13:13:26 +0300

    Fixed Withdrawal search

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,6 +14,7 @@ class User < ActiveRecord::Base
 
   has_many :to_payments, class_name: 'Payment', foreign_key: 'to_user_id'
   has_many :from_payments, class_name: 'Payment', foreign_key: 'from_user_id'
+  has_many :withdrawals
 
   has_one :wallet, dependent: :destroy
   after_create :create_wallet
